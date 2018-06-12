a154bb80040b8159b7cf4cd35420ff5f96c95462
Christer Ljung <cljung@users.noreply.github.com>
2018-06-12 16:35:20 +0200

    point vm ext to github

--- vm-create-cli-win.cmd
+++ vm-create-cli-win.cmd
@@ -7,7 +7,7 @@ az group create --location westeurope --name "%rgname%"
 az vm create --resource-group "%rgname%" --name "%vmname%" --image "UbuntuLTS" --admin-username "%USERNAME%" --admin-password "%vmpwd%" --use-unmanaged-disk --size Standard_D1_v2 --storage-account "%vmname%stg" --storage-sku "Standard_LRS"
 
 rem run a script inside the VM to complete the installation as we like it
-az vm extension set --resource-group "%rgname%" --vm-name "%vmname%" --name "customScript" --publisher "Microsoft.Azure.Extensions" --protected-settings "{'fileUris': ['https://cljungtest10.blob.core.windows.net/public/ubuntu-install-devtools.sh'],'commandToExecute': './ubuntu-install-devtools.sh %USERNAME%'}"
+az vm extension set --resource-group "%rgname%" --vm-name "%vmname%" --name "customScript" --publisher "Microsoft.Azure.Extensions" --protected-settings "{'fileUris': ['https://raw.githubusercontent.com/cljung/aztechdays/master/ubuntu-install-devtools.sh'],'commandToExecute': './ubuntu-install-devtools.sh %USERNAME%'}"
 
 rem (AKS) the docker app running in the VM  
 az network nsg rule create --resource-group "%rgname%" --nsg-name "%vmname%NSG" --name "Port_80" --access allow --protocol Tcp --direction Inbound --priority 110 --source-address-prefix "*" --source-port-range "*" --destination-address-prefix "*" --destination-port-range 80
@@ -27,7 +27,7 @@ rem sudo gpasswd -a $USER docker
 rem
 rem then do, logout and login again so than group membership takes effect. Then do
 rem 
-rem wget https://cljungtest10.blob.core.windows.net/public/azure-container-services-tutorial.sh
+rem wget https://raw.githubusercontent.com/cljung/aztechdays/master/azure-container-services-tutorial.sh
 rem chmod +x azure-container-services-tutorial.sh
 rem dos2unix azure-container-services-tutorial.sh
 
