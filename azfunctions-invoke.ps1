eb0e003c230b08ff0eb8ff6468c74241d1ffb3ea
cljung <cljung@live.se>
2018-06-13 10:33:12 +0200

    made generic

--- azfunctions-invoke.ps1
+++ azfunctions-invoke.ps1
@@ -1,9 +1,9 @@
-$uri1 = "https://cljungfunc02.azurewebsites.net/api/HttpTriggerCSharp1?code=WfmA5L6rzACdBl1hwUBIjc1ON6wLm8HTeCbgxI/KjmayBusWyqGSWw=="
+$uri1 = "https://<funcappname>.azurewebsites.net/api/HttpTriggerCSharp1?code=...key..."
 Invoke-RestMethod -Uri "$($uri1)&name=There!" -Method Get 
 
 
-$uri2 = "https://cljungfunc02.azurewebsites.net/api/HttpTriggerCSharp2?code=N4awoUdaiyOSAyT4RuThYEk30Uxx/bm4eesIdVgC8t29GLUqLaZNlQ=="
+$uri2 = "https://<funcappname>.azurewebsites.net/api/HttpTriggerCSharp2?code=...key..."
 $body = @{name = 'Max Power'}
 Invoke-RestMethod -Uri $uri2 -method Post -Body ($body | ConvertTo-json)
 
-curl -G "https://cljungfunc02.azurewebsites.net/api/HttpTriggerCSharp1?code=WfmA5L6rzACdBl1hwUBIjc1ON6wLm8HTeCbgxI/KjmayBusWyqGSWw==&curl"
+curl -G "https://<funcappname>.azurewebsites.net/api/HttpTriggerCSharp1?code=...key..."
